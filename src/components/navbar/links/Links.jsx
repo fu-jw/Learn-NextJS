43069a1bdbc505b3aeb63e12fa40e1dfeb436577
fu-jw <contact@fu-jw.com>
2024-06-27 11:04:51 +0800

    激活页面样式

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import styles from "./links.module.css"
+import NavLink from "./navLink/navLink";
 
 const Links = () => {
 
@@ -25,7 +26,8 @@ const Links = () => {
   return (
     <div className={styles.links}>
       {links.map((link=>(
-        <Link href={link.path} key={link.title}>{link.title}</Link>
+        // <Link href={link.path} key={link.title}>{link.title}</Link>
+        <NavLink item={link} key={link.title}/>
       )))}
     </div>
   )
