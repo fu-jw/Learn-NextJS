dd32565041088c31c40e2a3b560b8704dbd0938c
fu-jw <contact@fu-jw.com>
2024-06-27 10:37:52 +0800

    新增样式

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
-import Navbar from '@/components/navbar/Navbar'
-import Footer from '@/components/footer/Footer'
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/navbar/Navbar";
+import Footer from "@/components/footer/Footer";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,10 +14,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-          {/* <h1>这是MainLayout, 会出现在每个页面</h1> */}
-          <Navbar />
-          {children}
-          <Footer/>
+          <div className="container">
+            <Navbar />
+            {children}
+            <Footer/>
+          </div>
         </body>
     </html>
   )
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,14 +1,14 @@
 // import Link from "next/link";
 import Links from "./links/Links";
+import styles from "./navbar.module.css"
 
 const Navbar = () => {
   return (
-    <div>
-      <div>Logo</div>
+    // <div className="container">
+    // 使用此方法{styles.container}，不用担心重名问题，NextJS会在名称后面自动增加随机字符
+    <div className={styles.container}>
+      <div className={styles.logo}>Logo</div>
       <div>
-        {/* <Link href={"/"}>HomePage</Link>
-        <Link href={"/about"}>About</Link>
-        <Link href={"/contact"}>Contact</Link> */}
         <Links/>
       </div>
     </div>
