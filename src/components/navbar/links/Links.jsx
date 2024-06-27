a9203969bebf65c133eb66008dcf4a9f47e6fddf
fu-jw <contact@fu-jw.com>
2024-06-27 11:31:05 +0800

    增加两个按钮：admin，logout

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,6 +1,7 @@
-import Link from "next/link";
+// import Link from "next/link";
 import styles from "./links.module.css"
 import NavLink from "./navLink/navLink";
+
 
 const Links = () => {
 
@@ -23,12 +24,30 @@ const Links = () => {
     },
   ];
 
+  // 临时变量
+  // 是否登录
+  const session = true;
+  // 是否是管理员
+  const isAdmin = true;
+
+
+
   return (
     <div className={styles.links}>
       {links.map((link=>(
         // <Link href={link.path} key={link.title}>{link.title}</Link>
         <NavLink item={link} key={link.title}/>
       )))}
+      {session ? (
+        <>
+          {isAdmin && <NavLink item={{title:"Admin",path:"/admin"}}/>}
+          <button className={styles.logout}>Logout</button>
+        </>
+
+      ) : (
+        <NavLink item={{title:"Login",path:"/login"}}/>
+      )}
+
     </div>
   )
 }
